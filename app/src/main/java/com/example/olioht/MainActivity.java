0ddfd1cd45d6c563999a33248d4d36fa5861a37a
ilmari99 <hai4liez>
2020-05-07 18:12:32 +0300

    MItä vattua

--- app/src/main/java/com/example/olioht/MainActivity.java
+++ app/src/main/java/com/example/olioht/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        date = null;
 
         // Setting up calendar
         calendarview = findViewById(R.id.calendarView);
